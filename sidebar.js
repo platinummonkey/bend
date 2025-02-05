27a2d774db85e2b8fa351b1fbb2d048d3a667d8a
Nisarg Kolhe <nisargkolhe@gmail.com>
2025-02-05 14:36:40 -0500

    Change favicon size to 96

--- sidebar.js
+++ sidebar.js
@@ -36,7 +36,7 @@ function generateUUID() {
 function faviconURL(u) {
   const url = new URL(chrome.runtime.getURL("/_favicon/"));
   url.searchParams.set("pageUrl", u);
-  url.searchParams.set("size", "16");
+  url.searchParams.set("size", "96");
   return url.toString();
 }
 
